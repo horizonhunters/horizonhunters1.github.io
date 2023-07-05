7b79d5570d5231ad3866a412039fce09a8a5080d
horizonhunters <137650152+horizonhunters@users.noreply.github.com>
2023-07-05 17:25:03 -0600

    Add files via upload

---
+++ carrusel.js
@@ -0,0 +1,4 @@
+var carousel = document.getElementById("carouselExampleControls");
+var carouselInstance = new bootstrap.Carousel(carousel, {
+  interval: 35000 // Establece el intervalo en 5 segundos
+});
